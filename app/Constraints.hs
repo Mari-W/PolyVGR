f5ee8f48b340c4130afc0d60cb5ff3aff6cf7bb4
Mari <weidner@cs.uni-freiburg.de>
2022-10-11 21:44:43 +0200

    [ constraints ] fix point search

--- app/Constraints.hs
+++ app/Constraints.hs
@@ -6,8 +6,6 @@ import Context
 import Equality
 import Data.List
 
-type Disjoint = (String, String)
-
 stateDom :: Type -> Result Type
 stateDom SSEmpty = ok DEmpty 
 stateDom (SSBind d s) = ok d 
@@ -18,19 +16,40 @@ stateDom (SSMerge l r) = do
   ok (DMerge dl dr)
 stateDom t = raise ("[CE] expected state to extract dom of, got " ++ show t)
 
-splitCstr :: Cstr -> [Disjoint]
-splitCstr (DEmpty, _) = []
-splitCstr (DProj l d, d') = case d of
-  DMerge dl dr -> case l of
-    LLeft -> splitCstr (dl, d')
-    LRight -> splitCstr (dr, d')
-  _ -> error "unreachable"
-splitCstr (DMerge d d', d'') = splitCstr (d, d'') ++ splitCstr (d', d'')
-splitCstr (TVar i, TVar j) = [(i, j)]
-splitCstr (d, d') = splitCstr (d', d)
+splitDom :: Type -> Result [Type]
+splitDom DEmpty = ok []
+splitDom (DMerge d d') = do
+  xs <- splitDom d 
+  ys <- splitDom d'
+  ok (xs ++ ys)
+splitDom (DProj l d) = ok [DProj l d]
+splitDom (TVar x) = ok [TVar x]
+splitDom t = raise ("[CE] expected state to extract dom of, got " ++ show t)
 
-splitCstrs :: [Cstr] -> [Disjoint]
-splitCstrs = concatMap splitCstr
+splitCstr :: Cstr -> Result [Cstr]
+splitCstr (a, b) = do
+  xs <- splitDom (tNf a)
+  ys <- splitDom (tNf b)
+  ok (combs xs ys ++ combs ys xs)
+
+splitCstrs :: [Cstr] -> Result [Cstr]
+splitCstrs x = fmap concat (mapM splitCstr x)
+
+invLabel :: Label -> Label 
+invLabel LLeft = LRight 
+invLabel LRight = LLeft 
+
+extCstrs :: [Cstr] -> [Cstr]
+extCstrs cs = concatMap f cs where
+  f (DProj l d, t) = case find (\ (t1, t2) -> t1 == DProj (invLabel l) d && t2 == t) cs of
+    Nothing -> []
+    Just _ -> [(d, t), (t, d)]
+  f _ = []
+  
+fixExtCstrs :: [Cstr] -> [Cstr]
+fixExtCstrs cs = do 
+  let ext = extCstrs cs
+  if null ext then cs else fixExtCstrs (ext ++ cs)
 
 filterCstrs :: Ctx -> [Cstr]
 filterCstrs [] = []
@@ -38,23 +57,27 @@ filterCstrs (x : xs) = case x of
   (_, HasCstr c) -> c : filterCstrs xs
   _ -> filterCstrs xs
 
-searchCstr :: [Disjoint] -> Disjoint -> Result ()
+searchCstr :: [Cstr] -> Cstr -> Result ()
 searchCstr [] _ = raise "[CE] constraint not resolved"
 searchCstr ((x, y) : xs) (a, b) = do
-  if (x == a && y == b) || (x == b && y == a) then
+  if x == a && y == b then
     ok ()
   else searchCstr xs (a, b)
 
-searchCstrs :: [Disjoint] -> [Disjoint] -> Result ()
+searchCstrs :: [Cstr] -> [Cstr] -> Result ()
 searchCstrs atms (x : xs) = do
   searchCstr atms x
   searchCstrs atms xs
 searchCstrs atms [] = ok ()
 
+ce :: Ctx -> [Cstr] -> Result ()
+ce ctx cs = do
+  assm <- fixExtCstrs <$> splitCstrs (filterCstrs ctx)
+  cstrs <- splitCstrs cs
+  searchCstrs assm cstrs
+
 statesDisjunct :: Ctx -> Type -> Type -> Result ()
 statesDisjunct ctx ssl ssr = do
   dssl <- stateDom ssl
   dssr <- stateDom ssr
-  let cstrs = splitCstr (dssl, dssr)
-  let assm = splitCstrs (filterCstrs ctx)
-  searchCstrs assm cstrs
+  ce ctx [(dssl, dssr)]
